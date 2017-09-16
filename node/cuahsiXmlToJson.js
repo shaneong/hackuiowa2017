96c3a725dfa5e6d8f1f6617be78914dfd3f36c1b
shaneong <ongxx103@umn.edu>
2017-09-16 15:24:54 -0500

    backend api call

--- node/cuahsiXmlToJson.js
+++ node/cuahsiXmlToJson.js
@@ -29,10 +29,46 @@ request.post(
     endDate: ''
   } },
   function (error, response, body) {
-      var parser = require('xml2json');      
-      var json = parser.toJson(body);
+      var parser = require('xml-js');      
+      var json = parser.xml2json(body, {compact: false, spaces: 2 });
       console.log(json);
-
   }
 );
 
+request.post(
+  'http://hiscentral.cuahsi.org/webservices/hiscentral.asmx/GetSeriesMetadataCountOrData',
+  { form: {
+    getData: true,
+    getFacetOnCV: true,
+    xmin: '10',
+    xmax: '100',
+    ymin: '10',
+    ymax: '100',
+    sampleMedium: '',
+    dataType: '',
+    valueType: '',
+    generalCategory: '',
+    conceptKeyword: '',
+    networkIDs: '',
+    beginDate: '',
+    endDate: ''
+  } },
+  function (error, response, body) {
+      var parser2 = require('xml-js');      
+      var json = parser2.xml2json(body, {compact: false, spaces: 2 });
+      console.log(json);
+  }
+);
+
+request.post(
+  'https://waterservices.usgs.gov/nwis/iv/',
+  { form: {
+    sites: '01646500'
+  } },
+  function (error, response, body) {
+    // console.log(body);
+    var parser2 = require('xml-js');      
+    var json = parser2.xml2json(body, {compact: false, spaces: 2 });
+    console.log(json);
+  }
+);
