2963441a1b6ede2b40cad5b20818c3b5963166c5
Thomas <thomas@Thomass-MacBook-Air.local>
2017-09-17 10:26:29 -0500

    machine learning

--- node/machine_learning.js
+++ node/machine_learning.js
@@ -1,82 +1,72 @@
 const request = require('request');
 const fs = require('fs');
 var parse = require('csv-parse');
-
-
 var brain = require('brain');
 var net = new brain.NeuralNetwork();
 
-
-var url = 'https://nwis.waterservices.usgs.gov/nwis/iv/?sites=05454500&format=json&parameterCd=00060,00065&startDT=2006-01-01&endDT=2008-12-31';
-
-
-var html = '';
-var sum = 0;
+var url = 'https://nwis.waterservices.usgs.gov/nwis/iv/?sites=05454500&format=json&parameterCd=00060,00065,00045&startDT=2006-01-01&endDT=2008-12-31';
 var j = 0;
 var precipitations = [];
 var values = [];
-var predictData = [];
+var predictDataYearOne = [];
+var predictDataYearTwo = [];
+var predictDataYearThree = [];
+var trainingOutput = [];
+var dayOfYear = 1;
 
 
 request(url, (error, response, body)=> {
     if (!error && response.statusCode === 200) {
         const json = JSON.parse(body);
         for(var i in json.value.timeSeries[0].values[0].value) {
-            // values.push(JSON.stringify(json.value.timeSeries[0].values[0].value[i].value));
+            // console.log(JSON.stringify(json.value.timeSeries[0].values[0].value[i].value));
             if(JSON.stringify(json.value.timeSeries[0].values[0].value[i].dateTime).split('\"')[1].endsWith('12:00:00.000-06:00') ||
                     JSON.stringify(json.value.timeSeries[0].values[0].value[i].dateTime).split('\"')[1].endsWith('12:00:00.000-05:00')) { //
                 values[j] = JSON.stringify(json.value.timeSeries[0].values[0].value[i].value).split('\"')[1] * 10 / 10;
-                // console.log(JSON.stringify(json.value.timeSeries[0].values[0].value[i].dateTime));
-                // console.log(values[j]);
+                // console.log(j," " , JSON.stringify(json.value.timeSeries[0].values[0].value[i].dateTime));
+
+                trainingOutput[j] = 0;
+
+                if(values[j] > 8000) {
+                    // console.log(values[j]);
+                    trainingOutput[j] = 1;
+                }
                 j++;
             }
         }
 
         for(i in values) {
-            var obj = {};
-            // console.log(precipitations[i]);
-            obj.input = precipitations[i];
-
-            // console.log(values[i]);
-            obj.output = values[i];
-            predictData.push(obj);
+            if(dayOfYear > 365) {
+                dayOfYear -= 365;
+            } else {
+                dayOfYear++;
+            }
+            if((i > 869 && i < 894) || trainingOutput[i] === 1 || precipitations[i] > 26) {
+            //if(i > 0 && i < 1400) {
+                predictDataYearOne.push({input: [dayOfYear, precipitations[i], values[i]], output: [5]});
+            } else {
+                predictDataYearOne.push({input: [dayOfYear, precipitations[i], values[i]], output: [0]});
+            }
         }
 
-        // console.log(JSON.stringify(predictData));
+        console.log(JSON.stringify(predictDataYearOne));
 
-        var trainStream = net.createTrainStream({
-            /**
-            * Write training data to the stream. Called on each training iteration.
-            */
-            floodCallback: function() {
-                flood(trainStream, predictData);
-                console.log("training...");
-            },
-
-            /**
-            * Called when the network is done training.
-            */
-            doneTrainingCallback: function(obj) {
-                console.log("trained in " + obj.iterations + " iterations with error: "
-                            + obj.error);
-
-                var result = net.run(12);
-
-                console.log("Result: ", result);  // 0.987
-            }
+        net.train(predictDataYearOne, {
+            errorThresh: 0.05,  // error threshold to reach
+            iterations: 20000,   // maximum training iterations
+            log: true,           // console.log() progress periodically
+            logPeriod: 10,       // number of iterations between logging
+            learningRate: 0.3    // learning rate
         });
 
-                // kick it off
-        flood(trainStream, predictData);
 
-
-        function flood(stream, data) {
-          for (var i = 0; i < data.length; i++) {
-            // stream.write(data[i]);
-          }
-          // let it know we've reached the end of the data
-          // stream.write(null);
-        }
+        //for(var i = 0; i < 20000; i++) {
+           var output = net.run([200, 1, 10000]); // [precipitation, river water depth]
+           console.log('result: ', output[0]);
+           // output = net.run([10, 10000]); // [precipitation, river water depth]
+        //   console.log("tested data: 0.3, ", i)
+           // console.log('result: ', output[0]);
+        //}
 
     } else {
         console.log("Got an error: ", error, ", status code: ", response.statusCode);
@@ -91,22 +81,3 @@ fs.readFile('./res/iowa_city_precipitation.csv', function (err, fileData) {
         }
     });
 })
-
-//
-// var url = 'https://nwis.waterservices.usgs.gov/nwis/iv/?sites=05454500&format=json&parameterCd=00060,00065&startDT=2006-11-22&endDT=2008-12-22';
-//
-// for(var i in json.value.timeSeries[0].values[0].value) {
-//     html += JSON.stringify(json.value.timeSeries[0].values[0].value[i].value);
-//     html += '\n';
-//     // values.push(JSON.stringify(json.value.timeSeries[0].values[0].value[i].value));
-//     numberHolder = JSON.stringify(json.value.timeSeries[0].values[0].value[i].value);
-//     sum += numberHolder;
-//     j++
-//     if(j === 45) {
-//         j = 0;
-//         values[i] = sum / 46;
-//         sum = 0;
-//     }
-// }
-//
-// $('#json').html(html);
